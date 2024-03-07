3156f3f430610a49bb6d72c6a89813985a465182
Doan Trong Hieu <harrisondoan2810@gmail.com>
2024-03-08 05:32:21 +0700

    Update

--- FastAPI/books/BDSAWFAPI_F.Voron/03_RESTful-API/first_endpoint.py
+++ FastAPI/books/BDSAWFAPI_F.Voron/03_RESTful-API/first_endpoint.py
@@ -1,7 +1,9 @@
 # first_endpoint.py
 from enum import Enum
-from fastapi import Body, FastAPI, Path, Query
+from fastapi import Body, Cookie, FastAPI, File, Form, Header, Path, Query, Request, UploadFile
 from pydantic import BaseModel
+from typing import Union
+
 #*-----------------------------------------------------------------------------
 
 # Define string enumeration by inheriting from str type and Enum class.
@@ -19,6 +21,7 @@ class User(BaseModel):
   age: int
 
 class Company(BaseModel):
+  # Pydantic model with a single string name property.
   name: str
 #*-----------------------------------------------------------------------------
 
@@ -90,8 +93,93 @@ async def get_user(
 # argument `user` for path operation function with User class as type hint.
 # FastAPI understands `user` data in request payload.
 # Access user object instance's properties using dot notation (user.name).
-async def create_user(user: User):
+async def create_user(
+  user: User, 
+  company: Company,
+  # Singular body values for a single property not part of any model.
+  # integer between 1 and 3
+  priority: int = Body(..., ge=1, le=3),
+):
   return {
     # FastAPI automatically converts the object into JSON for the HTTP response.
-    user
+    "user": user,
+    "company": company,
+    "priority": priority,
   }
+
+@app.post('/posts')
+# The Content-Type header and body data representation have changed in the request.
+# The response in JSON. FastAPI always output a JSON response
+async def create_post(
+  # FastAPI does not allow defining Pydantic models for form data validation. 
+  # Fields must be manually defined as arguments for the path operation function.
+  title: str = Form(...),
+  page: int = Form(...),
+):
+  return {
+    "title": title,
+    "page": page,
+  }
+  
+@app.post("/files")
+async def upload_file(file: UploadFile = File(...)):
+  # UploadFile class stores data in memory up to a threshold and then 
+  # automatically store it on disk. 
+  # The class exposes metadata like content type and a file-like interface for
+  # manipulation
+  return {
+    "file_name": file.filename,
+    "content_type": file.content_type,
+  }
+
+@app.post("/files_multiple")
+async def upload_multiple_files(files: list[UploadFile] = File(...)):
+  return [
+    {
+      "file_name": file.filename, "content_type": file.content_type,
+    }
+    for file in files
+  ]
+
+@app.get("/headers/hello")
+async def get_header_hello(
+  hello: str = Header(...),
+):
+  # Use the Header function as a default value for the hello argument. 
+  # The name of the argument determines the key of the header to retrieve.
+  # FastAPI retrieved the header value without a default value specified, 
+  # making the header required. 
+  # If missing, a 422 status error response will be returned.
+  return {
+    "hello": hello,
+  }
+
+@app.get("/headers/user-agent")
+async def get_header_user_agent(
+  # FastAPI automatically converts header names into lowercase and snake case, 
+  # making it compatible with any valid Python variable name.
+  # User agent is an HTTP header added automatically by most HTTP clients and 
+  # web browsers to identify the application making the request. 
+  # Web servers use this information to adapt the response.
+  user_agent: str = Header(...),
+):
+  return {
+    "user_agent": user_agent,
+  }
+
+@app.get("/cookies")
+async def get_cookie(
+    # Set default value None to Cookie function. Proceed without generating 
+    # 422 status error response if cookie not set in request.
+    hello: Union[str, None] = Cookie(None)
+):
+  return {
+    "hello": hello,
+  }
+
+@app.get("/requests")
+async def get_request_object(request: Request):
+  return {
+    "path": request.url.path,
+  }
+
